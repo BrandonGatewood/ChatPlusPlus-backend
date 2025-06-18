bf3eef078d3943fa14227a5c216c26f4b16d4b55
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2025-06-18 15:08:00 -0700

    small changed to expiration, and setting date

--- app/core/jwt.py
+++ app/core/jwt.py
@@ -6,11 +6,11 @@ from fastapi import HTTPException, status
 # Replace with your secret key; keep it safe!
 SECRET_KEY = "your_secret_key_here"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    expire = datetime.now() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
