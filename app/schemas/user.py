ee1092a147a1f93c8767d3d4783acc80408840b7
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2025-06-18 15:07:02 -0700

    removed userresponse class and added token schema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -5,13 +5,6 @@ class UserCreate(BaseModel):
     email: EmailStr  # validates email format
     password: str    # plain password input
 
-# Schema for user response (what we send back to client)
-class UserResponse(BaseModel):
-    id: int
-    email: EmailStr
-
-    class Config:
-        orm_mode = True  # tells Pydantic to read data from ORM objects
 
 # Schema for user login
 class UserLogin(BaseModel):
