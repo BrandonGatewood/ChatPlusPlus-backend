913d66b841cfb6cb331fd5569d78b23712d20ea8
Brandon Gatewood <brandongatewood9@gmail.com>
2025-06-18 20:09:05 -0700

    frontend url is in .env

--- app/main.py
+++ app/main.py
@@ -1,18 +1,25 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.db.models.base import Base
+from app.db.session import engine
 from app.api.routes import auth 
 from app.api.routes import user
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React dev server
+    allow_origins=[os.getenv("FRONTEND_URL")],  # React dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 # Include auth router with prefix /auth
-app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(auth.router, tags=["auth"])
 app.include_router(user.router, prefix="/user", tags=["user"])
