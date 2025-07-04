e6b2b40389285a9d280c9594b5ad0f1425c60348
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2025-07-04 16:35:33 -0700

    get chat + messages w eagerloading

--- app/crud/crud_chat.py
+++ app/crud/crud_chat.py
@@ -1,6 +1,7 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from app.db.models.chat import Chat
 from app.db.models.user import User 
+from app.crud.crud_message import get_messages_for_chat
 
 def create_chat(db: Session, user_id: int, title: str) -> Chat:
     user = db.query.filter(User.id == user_id).first()
@@ -20,3 +21,18 @@ def delete_chat(db: Session, chat_id: int) -> None:
     
     db.delete(chat)
     db.commit()
+
+def get_chat(db: Session, chat_id: int) -> Chat:
+    chat = (
+        db.query(Chat)
+        .options(joinedload(Chat.messages))
+        .filter(Chat.id == chat_id)
+        .first()
+    )
+
+    if not chat:
+        raise ValueError("Chat with id {chat_id} not found")
+    return chat
+    
+
+#def get_all_chat_titles(db: Session, user_id: int) -> 
