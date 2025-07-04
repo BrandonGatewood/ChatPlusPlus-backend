e1dcb95c7bf04a060c8f21654b0f1515562ea89a
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2025-07-04 16:56:28 -0700

    implemented edit_message function editing a message will update the message and delete all messages that follow it

--- app/crud/crud_message.py
+++ app/crud/crud_message.py
@@ -1,3 +1,4 @@
+import datetime
 from sqlalchemy.orm import Session
 from app.db.models.message import Message
 from app.db.models.chat import Chat
@@ -13,4 +14,20 @@ def add_message(db: Session, chat_id: int, sender: str, text: str) -> Message:
     db.refresh(message)
     return message
 
-#def edit_message(db: Session, chat_id)
+def edit_message(db: Session, message_id: int, text: str) -> Message: 
+    with db.begin():
+        message = db.query(Message).filter(Message.id == message_id).first()
+        if not message:
+            raise ValueError(f"Chat with id {message_id} not found")
+
+        message.text = text
+        _delete_messages_after_edit(Session, message.chat_id, message.created_at)
+
+    db.refresh(message) 
+    return message
+
+def _delete_messages_after_edit(db: Session, chat_id: int, edited_message_created_at: datetime) -> None:
+    db.query(Message).filter(
+        Message.chat_id == chat_id,
+        Message.created_at > edited_message_created_at
+    ).delete(synchronize_session=False)
