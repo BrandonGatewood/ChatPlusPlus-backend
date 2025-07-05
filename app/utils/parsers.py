3aa7b9a9deceb0fc31c8661fcbb489ea1e0a4164
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2025-07-05 01:12:58 -0700

    both parsers return MessageRequest

--- app/utils/parsers.py
+++ app/utils/parsers.py
@@ -1,12 +1,13 @@
 from io import BytesIO
 from pdfminer.high_level import extract_text as extract_pdf_text
 import docx
+from app.schemas.schema_message import MessageRequest
 
-def parse_pdf_bytes(file_bytes: bytes) -> str:
+def parse_pdf_bytes(file_bytes: bytes) -> MessageRequest:
     """Extract text from a PDF file given as bytes."""
-    return extract_pdf_text(BytesIO(file_bytes))
+    return MessageRequest(text= extract_pdf_text(BytesIO(file_bytes)))
 
-def parse_docx_bytes(file_bytes: bytes) -> str:
+def parse_docx_bytes(file_bytes: bytes) -> MessageRequest:
     """Extract text from a DOCX file given as bytes."""
     doc = docx.Document(BytesIO(file_bytes))
-    return "\n".join([p.text for p in doc.paragraphs])
+    return MessageRequest(text="\n".join([p.text for p in doc.paragraphs]))
