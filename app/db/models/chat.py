08c929f8fb734a25509565d4aed13fbb47ea52e8
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2025-07-05 16:49:24 -0700

    updated chat model changed ids to UUID all columns are non nullable added on_delete="Cascade" to user_id, deletes all chats with user_id added passive_deletes=True to messages relationship, deletes more efficient by relying on db cascade

--- app/db/models/chat.py
+++ app/db/models/chat.py
@@ -1,19 +1,31 @@
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, func
 from sqlalchemy.orm import relationship
 from app.db.models.base import Base  # Base class for all models, handles SQLAlchemy ORM setup
 
 """
-SQLAlchemy Message model definition, representing a chat with messages.
+SQLAlchemy Chat model definition, representing a chat with messages.
 """
 
 class Chat(Base):
     __tablename__ = "chats"
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        nullable=False
+    )
+    user_id = Column(
+        UUID(as_uuid=True), 
+        ForeignKey("users.id", ondelete="CASCADE"),
+        index=True,
+        nullable=False
+    )
     title = Column(String(255), nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=False)
 
     user = relationship("User", back_populates="chats")
-    messages = relationship("Message", back_populates="chat", cascade="all, delete-orphan")
+    messages = relationship("Message", back_populates="chat", cascade="all, delete-orphan", passive_deletes=True)
