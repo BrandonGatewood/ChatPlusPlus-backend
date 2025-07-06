788f6658a9201a41f2c340a0569aa48e04ec7075
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2025-07-06 12:32:20 -0700

    added comments

--- app/utils/parsers.py
+++ app/utils/parsers.py
@@ -4,10 +4,26 @@ import docx
 from app.schemas.schema_message import MessageRequest
 
 def parse_pdf_bytes(file_bytes: bytes) -> str:
-    """Extract text from a PDF file given as bytes."""
+    """
+    Extract text from a PDF file given as bytes.
+
+    Args:
+        file_bytes: The binary contents of a PDF file
+
+    Returns:
+        The extracted file as string
+    """
     return extract_pdf_text(BytesIO(file_bytes))
 
 def parse_docx_bytes(file_bytes: bytes) -> str:
-    """Extract text from a DOCX file given as bytes."""
+    """
+    Extract text from a DOCX file given as bytes.
+    
+    Args:
+        file_bytes: The binary contents of a Docx file
+
+    Returns:
+        The extracted file as string
+    """
     doc = docx.Document(BytesIO(file_bytes))
     return "\n".join([p.text for p in doc.paragraphs])
