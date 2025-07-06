d1f60bd0107abf5ab8b44e4449ae621b469ce353
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2025-07-06 13:14:13 -0700

    replaced depreciated import

--- app/db/models/base.py
+++ app/db/models/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 # This creates a base class that our ORM models will inherit from.
 # It sets up the SQLAlchemy declarative system.
