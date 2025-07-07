dd4566cdef1bf3452c48ffe68f28a4bafe7cc900
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2025-07-07 09:17:51 -0700

    updated comments

--- app/crud/crud_message.py
+++ app/crud/crud_message.py
@@ -12,30 +12,36 @@ Includes adding new messages, editing messages, and deleting future messages
 after an edit to maintain chat consistency.
 """
 
-def add_message(db: Session, chat_id: UUID, sender: str, text: str) -> None:
+def add_message(
+        db: Session,
+        chat_id: UUID,
+        sender: str,
+        text: str
+) -> None:
     """
-    Add a new message to the given chat.
+    Add a new message to the chat.
 
     Args:
         db: SQLAlchemy DB session.
-        chat_id: The chat's unique ID.
-        sender: sender of message.
+        chat_id: The unique UUID for the chat.
+        sender: The sender of message.
         text: the sender's message text.
 
     Raises:
-        ChatNotFoundError: if chat_id does not exist.
+        ChatNotFoundError: If chat does not exist.
 
     Returns:
         None.
     """
     chat = db.query(Chat).filter(Chat.id == chat_id).one_or_none()
     if not chat:
-        raise ChatNotFoundError(f"Chat with id {chat_id} not found")
+        raise ChatNotFoundError(f"Chat with id {chat_id} not found.")
 
     message = Message(chat_id=chat_id, sender=sender, text=text)
 
     db.add(message)
     db.refresh(message)
+
 
 def edit_message(db: Session, message_id: UUID, text: str) -> None: 
     """
@@ -43,31 +49,32 @@ def edit_message(db: Session, message_id: UUID, text: str) -> None:
 
     Args:
         db: SQLAlchemy DB session.
-        message_id: The message's unique ID.
-        text: the updated message text.
+        message_id: The unique UUID for the message.
+        text: The updated message text.
 
     Raises:
-        MessageNotFoundError: if message_id does not exist.
+        MessageNotFoundError: If message does not exist.
 
     Returns:
         None.
     """
     message = db.query(Message).filter(Message.id == message_id).one_or_none()
     if not message:
-        raise MessageNotFoundError(f"Message with id {message_id} not found")
+        raise MessageNotFoundError(f"Message with id {message_id} not found.")
 
     message.text = text
 
     _delete_messages_after_edit(db, message.chat_id, message.created_at)
 
+
 def _delete_messages_after_edit(db: Session, chat_id: UUID, edited_message_created_at: datetime.datetime) -> None:
     """
-    Deletes all messages that were after the edited message.
+    Delete all messages that were after the edited message.
 
     Args:
         db: SQLAlchemy DB session.
-        chat_id: The chat's unique ID.
-        edited_message_created_at: the date and time when the original message was created.
+        chat_id: The unique UUID for the chat.
+        edited_message_created_at: The date and time when the original message was created.
 
     Returns:
         None.
