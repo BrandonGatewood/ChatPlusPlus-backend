c8f812ba0b2237e9b4b469fc83a605ff5e45945f
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2025-07-14 11:09:25 -0700

    implemented websocket connection to stream replies

---
+++ app/api/routes/websocket.py
@@ -0,0 +1,71 @@
+from uuid import UUID
+import uuid
+from fastapi import APIRouter, Depends, WebSocket, WebSocketDisconnect
+from sqlalchemy.orm import Session
+from app.crud.crud_chat import chat_ownership
+from app.exceptions import AuthorizationError, NotFoundError
+from app.core.auth import get_current_user_ws
+from app.db.session import get_db
+from app.services.services_websocket import build_prompt_service, stream_bot_response_service
+
+router = APIRouter()
+
+@router.websocket("/chat")
+async def websocket_endpoint(
+    websocket: WebSocket,
+    db: Session = Depends(get_db),
+) -> None:
+    """
+    Endpoint for streaming bot response.
+
+    Args:
+        websocket: The websocket connection.
+        db: The SQLAlchemy DB session.
+    
+    Closes:
+        Code=1008: Invalid chat id query.
+        Code=1008: Invalid chat id.
+        Code=1008: Invalid message id query.
+        Code=1008: Invalid message id.
+
+    Returns:
+        None.
+    """
+    try:
+        await websocket.accept()
+
+        # Get and validate chat_id
+        chat_id_str = websocket.query_params.get("chat_id")
+        if not chat_id_str:
+            await websocket.close(code=1008, reason="Invalid chat id query.")
+            return
+        try:
+            chat_id = uuid.UUID(chat_id_str)
+        except ValueError:
+            await websocket.close(code=1008, reason="Invalid chat id.")
+            return
+        
+        message_id_str = websocket.query_params.get("message_id")
+        if not message_id_str:
+            await websocket.close(code=1008, reason="Invalid message id query.")  # Policy Violation
+            return
+        try:
+            message_id = uuid.UUID(message_id_str)
+        except ValueError:
+            await websocket.close(code=1008, reason="Invalid message id.")
+            return
+
+
+
+        user_id = await get_current_user_ws(websocket, db)
+
+        if not chat_ownership(db, chat_id, user_id):
+            await websocket.close(code=1008, reason="Not Authorized.")
+            return
+
+        prompt = build_prompt_service(db, user_id.id, chat_id)
+        await stream_bot_response_service(websocket, db, message_id, prompt)
+
+    except WebSocketDisconnect:
+        print("Client disconnected") 
+    
---
+++ app/services/services_websocket.py
@@ -0,0 +1,93 @@
+import asyncio
+import json
+from fastapi import WebSocket
+from sqlalchemy.orm import Session
+from uuid import UUID
+from app.crud.crud_message import get_message
+from app.crud.crud_message import update_bot_message_text
+from app.services.services_chat import get_chat_service
+
+
+"""
+Service operations for websocket connection.
+
+Includes streaming llm response and building prompt to send to the llm.
+"""
+
+
+def llm_stream_sync(prompt: str):
+    # yields chunks synchronously
+    yield "Hello, "
+    yield "this is "
+    yield "a streamed "
+    yield "response!"
+
+
+async def stream_bot_response_service(
+    websocket: WebSocket,
+    db: Session,
+    message_id: UUID,
+    prompt: str
+) -> None:
+    """
+    stream the bots response. 
+
+    Args:
+        websocket: The websocket connection.
+        message_id: The unique UUID for the message.
+        prompt: the prompt to send the bot.
+
+    Closes:
+        Code=1000: Streaming complete.
+        Code=1008: Message not found.
+
+    Returns:
+        None.
+    """
+    message = get_message(db, message_id)
+
+    if not message:
+        print("no message obj")
+        await websocket.close(code=1008, reason="Message not found.")
+        return
+
+    for chunk in llm_stream_sync(prompt):
+        msg = {"id": str(message_id), "sender": "bot", "text": chunk}
+        await websocket.send_text(json.dumps(msg))
+
+        # Step 2: Update DB incrementally
+        update_bot_message_text(db, message, chunk)
+
+        await asyncio.sleep(0.5)
+    await websocket.close(code=1000, reason="Streaming complete.")
+
+
+def build_prompt_service(
+    db: Session,
+    user_id: UUID,
+    chat_id: UUID
+) -> str:
+    """
+    Build a prompt to send the bot.
+
+    Args:
+        db: SQLAlchemy DB session.
+        user_id: The unique UUID for the user.
+        chat_id: The unique UUID for the chat.
+    
+    Raises:
+        None.
+
+    Returns:
+        The newly created prompt.
+    """
+    chat = get_chat_service(db, chat_id, user_id)
+    messages = chat.messages if chat else []
+
+    prompt_lines = []
+    for message in messages:
+        prompt_lines.append(f"{message.sender} : {message.text}")
+    
+    prompt = "\n".join(prompt_lines)
+
+    return prompt
